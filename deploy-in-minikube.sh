8ded1ba0052a0bf5ba18fd0430abfb0ecde98e08
Tomas Ferrari <tferrari.92@gmail.com>
2024-03-07 13:33:40 -0300

    hola

--- deploy-in-minikube.sh
+++ deploy-in-minikube.sh
@@ -7,23 +7,37 @@ read -p "What image tag should be used for Backstage deployment?: " IMAGE_TAG
 # Start cluster. Extra beefy beause Backstage is a bit heavy.
 minikube start --cpus 4 --memory 4096
 
+# Install ArgoCD
+helm install argocd -n argocd helm/infra/argo-cd --values helm/infra/argo-cd/values-custom.yaml --dependency-update --create-namespace
+
+# Get ArgoCD admin password
+until kubectl -n argocd get secret argocd-initial-admin-secret &> /dev/null; do
+  echo "Waiting for secret 'argocd-initial-admin-secret' to be available..."
+  sleep 3
+done
+echo "#############################################################################"
+echo "#############################################################################"
+echo "#############################################################################"
+echo " "
+echo "TO ACCESS THE ARGOCD DASHBOARD, RUN THE FOLLOWING COMMAND:"
+echo "kubectl port-forward svc/argocd-server -n argocd 8081:443"
+echo " "
+echo "user: admin"
+echo "password: $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)"
+echo " "
+echo "#############################################################################"
+echo "#############################################################################"
+echo "#############################################################################"
+
+# Then we create an application that will monitor the helm/infra/argocd directory, the same we used to deploy ArgoCD, making ArgoCD self-managed. Any changes we apply in the helm/infra/argocd directory will be automatically applied.
+kubectl create -n argocd -f argo-cd/self-manage/argocd-application.yaml  
+
+# Finally, we create an application that will automatically deploy any ArgoCD Applications we specify in the argo-cd/applications directory (App of Apps pattern).
+kubectl create -n argocd -f argo-cd/self-manage/argocd-app-of-apps-application.yaml  
+
 # We create the secret for the Github token with this command. This way the token won't get pushed to Github.
 kubectl create ns backstage
 kubectl create secret generic github-token -n backstage --from-literal=GITHUB_TOKEN="$GITHUB_TOKEN"
-
-# Install Backstage
-helm install backstage -n backstage backstage/helm-chart --values backstage/helm-chart/values-custom.yaml --set-string backstage.image.tag="$IMAGE_TAG" --dependency-update --create-namespace
-
-# Install Redis
-kubectl create ns my-app
-kubectl apply -f k8s-manifests/my-app/redis
-
-# Install Backend service
-kubectl apply -f k8s-manifests/my-app/backend
-
-# Install Frontend service
-kubectl apply -f k8s-manifests/my-app/frontend
-
 # Wait for the Postgres pod to be ready
 echo "Waiting for postgres pod to be ready..."
 until [[ $(kubectl -n backstage get pods -l "app.kubernetes.io/name=postgresql" -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') == "True" ]]; do
